909e804ca062e7eb379001ee2a98ae513b42cf4d
JohnnyRodge <rodgers.j1@gmercyu.edu>
2024-05-01 18:12:56 -0400

    updated maps and dependent interiors

---
+++ ETC/Grocery_store_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Grocery_store_16x16" tilewidth="16" tileheight="16" tilecount="1248" columns="16">
+ <image source="../../../Desktop/dependent interiors/16_Grocery_store_16x16.png" width="256" height="1248"/>
+</tileset>
---
+++ dependent interiors/14_Basement_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="14_Basement_16x16" tilewidth="16" tileheight="16" tilecount="800" columns="16">
+ <image source="14_Basement_16x16.png" width="256" height="800"/>
+</tileset>
